f2c035f3f5974e936c17ad40652b55526aa1d971
maxime przybylo <przybylo.maxime@gmail.com>
2022-03-06 20:28:34 +0100

    register/login users. Modified templates with gabarits

--- bibliotheque/urls.py
+++ bibliotheque/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from  django.contrib.auth import views as auth_views
 from django.urls import include, path
 
 from . import views
@@ -21,7 +22,7 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name="index"),
-    path('browse/', include('browse.urls'))
+    path('browse/', include('browse.urls')),
 ]
 
 
